2b41f1b2c66613087c9262c06cf43d42c8158891
19evanmann <47829011+19evanmann@users.noreply.github.com>
2019-02-26 14:09:59 -0500

    Add files via upload

--- kinterexp.py
+++ kinterexp.py
@@ -1,46 +1,91 @@
 import tkinter as tk
+import turtle 
+t = turtle.Pen()
+t.speed(100)
 
-import turtle
+root = tk.Tk()
+def frame ():
+    tk.Frame(root)
+frame.pack()
 
-def right_spiral():
-    pen_color()
-    shape_size()
-    global siz_e
-    for x in range (siz_e):
-        t.width(1)
-        t.forward(x)
-        t.right(91)
 def left_spiral():
-    pen_color()
-    shape_size()
-    global siz_e
-    for x in range (siz_e):
+    for x in range (200):
         t.width(1)
         t.forward(x)
         t.left(91)
-def something():
-    pen_color()
-    shape_size()
-    global siz_e
-    for x in range (siz_e):
+
+def right_spiral():
+    for x in range (200):
         t.width(1)
         t.forward(x)
         t.right(91)
 
-root = tk.Tk()
-frame = tk.Frame(root)
-frame.pack()
+def something():
+    for x in range (200):
+        t.width(1)
+        t.forward(x)
+        t.right(187)
 
-button_1 = tk.Button(frame,
-                     text="Right Spiral",
-                     fg="green",
-                     cammand=right_spiral)
-button_1.pack(side+tk.LEFT)
+def color_red():
+    t.pencolor("Red")
 
-button_2 = tk.Button(frame,
-                     text="Left Spiral",
-                     fg="blue",
-                     comman=left_spiral)
-button_2.pack(side=tk.LEFT)
+def color_blue():
+    t.pencolor("Blue")
+
+def color_Medium():
+    t.pencolor("Medium Orchid")
+    
+def shapefr():
+    shapeframe = frame(root)
+    shapeframe.pack()
+
+def colorfr():
+    colorframe = frame(root)
+    colorframe.pack()
+
+
+#Shape Buttons
+
+button = tk.Button(shapefr(), 
+                   text="Left Squares", 
+                   command=left_spiral)
+button.pack(side=tk.LEFT)
+
+button2 = tk.Button(shapefr(),
+                   text="Right Squares",
+                   command=right_spiral)
+button2.pack(side=tk.LEFT)
+
+button3 = tk.Button(shapefr(),
+                    text="Spiral-gon",
+                    command=something)
+button3.pack(side=tk.LEFT)
+
+button4 = tk.Button(frame,
+                    text="Quit",
+                    command=quit)
+button4.pack(side=tk.BOTTOM)
+
+#Color Buttons
+
+red_b = tk.Button(colorfr(),
+                  text="Red",
+                  fg="Red",
+                  command=color_red)
+red_b.pack(sife=tk.LEFT)
+
+blue_b = tk.Button(colorfr(),
+                   text="Blue",
+                   fg="Blue",
+                   command=color_blue)
+blue_b.pack(side=tk.LEFT)
+
+medium_b = tk.Button(colorfr(),
+                     text="Medium Orchid",
+                     fg="Medium Orchid",
+                     command=color_Medium)
+medium_b.pack(side=tk.LEFT)
+
 
 root.mainloop()
+
